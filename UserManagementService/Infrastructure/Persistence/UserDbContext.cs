4ed0c417cae014104505e4946df1a6430ec83926
Phạm Thế Hiển <hien.pham@ezcloud.vn>
2025-05-29 00:39:28 +0700

    Add entity tbl_User

---
+++ UserManagementService/Infrastructure/Persistence/UserDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using UserManagementService.Domain.Entities;
+
+namespace UserManagementService.Infrastructure.Persistence;
+
+public partial class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<tbl_User> tbl_Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<tbl_User>(entity =>
+        {
+            entity.Property(e => e.UserId).ValueGeneratedNever();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- UserManagementService/Program.cs
+++ UserManagementService/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using UserManagementService.Services;
+using UserManagementService.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
-
+builder.Services.AddDbContext<UserDbContext>(
+        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
