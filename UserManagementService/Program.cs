a62cabb36b6d588dc80efab06400ffdc2fb056cc
Phạm Thế Hiển <hien.pham@ezcloud.vn>
2025-06-10 01:09:39 +0700

    chỉnh lại cấu hình

--- UserManagementService/Program.cs
+++ UserManagementService/Program.cs
@@ -4,13 +4,13 @@ using UserManagementService.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddGrpc();
 
-var app = builder.Build();
 builder.Services.AddDbContext<UserDbContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Configure the HTTP request pipeline.
+
+var app = builder.Build();
+
 app.MapGrpcService<UserService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
