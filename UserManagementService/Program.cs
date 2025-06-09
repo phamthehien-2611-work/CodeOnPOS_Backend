07c6d520667e58aaf6911b13c22cf13be61d5a26
Phạm Thế Hiển <hien.pham@ezcloud.vn>
2025-06-10 00:07:39 +0700

    Đăng ký, đăng nhập

--- UserManagementService/Program.cs
+++ UserManagementService/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 builder.Services.AddDbContext<UserDbContext>(
         options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UserService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
